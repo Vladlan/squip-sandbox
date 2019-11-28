34e62e29864ea981e9265daf310fa8ba5fac3102
Vladlan <vladislav_lanskikh@epam.com>
2019-11-28 19:38:17 +0300

    add readme.md

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ sqip({
     {
       name: 'sqip-plugin-primitive',
       options: {
-        numberOfPrimitives: 32,
+        numberOfPrimitives: 128,
         mode: 0,
       },
     },
     `sqip-plugin-svgo`,
   ],
+  // plugins: [
+  //   {
+  //     name: "primitive",
+  //     options: {
+  //       numberOfPrimitives: 128,
+  //       mode: 0
+  //     }
+  //   },
+  //   "svgo",
+  //   "data-uri"
+  // ]
 });
